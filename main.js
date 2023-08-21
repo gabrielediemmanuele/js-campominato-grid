91ecd9774b8e5b6ea5ebd6cdbc16152f2e7a3777
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-08-21 17:34:20 +0200

    feat: step 2.  Add eventListener

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 // Aggancio il bottone e la griglia a Java Script
 const button = document.getElementById("start-button");
 const grid = document.getElementById("grid");
+
+// Dò al bottone l'evento del click per generare la griglia.
+button.addEventListener("click", function () {
+  gridGenerator(); //** Aggancio alla funzione */
+});
